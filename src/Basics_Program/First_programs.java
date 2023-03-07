47cedb96493dc5387b92a1e6024296dd6b4a1ac6
Sheetal Jaswal <sheetal.jaswal@hcl.com>
2023-03-07 08:38:10 +0530

    first commit

---
+++ src/Basics_Program/First_programs.java
@@ -0,0 +1,11 @@
+package Basics_Program;
+
+public class First_programs {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("First Program");
+
+	}
+
+}
