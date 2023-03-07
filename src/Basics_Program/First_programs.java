40ae1717223c70ab6944a5a33419cafc4e84dbcb
Sheetal Jaswal <sheetal.jaswal@hcl.com>
2023-03-07 09:24:01 +0530

    third commit

--- src/Basics_Program/First_programs.java
+++ src/Basics_Program/First_programs.java
@@ -5,6 +5,8 @@ public class First_programs {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("First Programs");
+		System.out.println("First Programs");
+
 
 	}
 
